82a71bc2818065cccc87a6b7724b8f042dfe4118
Himanshu Kumar <himanshu@battlebucks.com>
2024-06-13 13:00:33 +0530

    Score init

--- CommonServer/src/DunesGame.ts
+++ CommonServer/src/DunesGame.ts
@@ -1,92 +1,91 @@
 
-let dune_waitingMap= new WaitingMatches<MatchMakingResponse>();
+let dune_waitingMap = new WaitingMatches<MatchMakingResponse>();
 const dune_gameName: string = 'Dunes';
-const dunne_Tag:string='TAG::Dunes';
+const dunne_Tag: string = 'TAG::Dunes';
 
-const Dune_CreateMatch: nkruntime.RpcFunction = function (ctx: nkruntime.Context, logger: nkruntime.Logger, nk: nkruntime.Nakama, payload: string): string
-{
+const Dune_CreateMatch: nkruntime.RpcFunction = function (ctx: nkruntime.Context, logger: nkruntime.Logger, nk: nkruntime.Nakama, payload: string): string {
     let responseJson = emptyResponse;
     let matchDetail: MatchMakingDetailsReceived | null = null;
-    try
-    {
+    try {
         matchDetail = JSON.parse(payload);
-    } catch (error)
-    {
+    } catch (error) {
         logger.error(`${dunne_Tag} Failed to deserialize JSON: ${error}`);
     }
-    if (matchDetail == null)
-    {
+    if (matchDetail == null) {
         return responseJson;
     }
-    else
-    {
+    else {
         let mmResponse = dune_waitingMap.GetMMRByRoomId(matchDetail.roomId);
-        if (mmResponse == undefined)
-        {
+        if (mmResponse == undefined) {
             //let param={roomName:matchDetail.roomId,minPlayerCount: matchDetail.minPlayerCount, maxPlayerCount: matchDetail.maxPlayerCount, autoDestroyRoom:matchDetail.autoDestroyRoom}
-            let matchId = nk.matchCreate(dune_gameName,matchDetail);
-            let mmData=new MatchMakingResponseData(matchDetail.roomId,matchId);
+            let matchId = nk.matchCreate(dune_gameName, matchDetail);
+            let mmData = new MatchMakingResponseData(matchDetail.roomId, matchId);
             mmResponse = new MatchMakingResponse(mmData);
             dune_waitingMap.Set(matchDetail.roomId, matchId, mmResponse);
         }
-        return  JSON.stringify(mmResponse);
+        return JSON.stringify(mmResponse);
     }
 }
 
-const Dune_MatchInit: nkruntime.MatchInitFunction<nkruntime.MatchState> = function (ctx: nkruntime.Context, logger: nkruntime.Logger, nk: nkruntime.Nakama, params: { [p: string]: any; }): { state: nkruntime.MatchState; tickRate: number; label: string; }
-{
+const Dune_MatchInit: nkruntime.MatchInitFunction<nkruntime.MatchState> = function (ctx: nkruntime.Context, logger: nkruntime.Logger, nk: nkruntime.Nakama, params: { [p: string]: any; }): { state: nkruntime.MatchState; tickRate: number; label: string; } {
     logger.warn(`TAG:MatchInit 1`);
-    let matchDetail:MatchMakingDetailsReceived=params as MatchMakingDetailsReceived;
+    let matchDetail: MatchMakingDetailsReceived = params as MatchMakingDetailsReceived;
     logger.warn(`TAG:MatchInit 2`);
-    let matchMeta= new MatchMateState(matchDetail.roomId,matchDetail.minPlayerCount,matchDetail.maxPlayerCount,0,matchDetail.matchMakeWaitTime,matchDetail.gamePlayTime);
+    let matchMeta = new MatchMateState(matchDetail.roomId, matchDetail.minPlayerCount, matchDetail.maxPlayerCount, 0, matchDetail.matchMakeWaitTime, matchDetail.gamePlayTime);
     let currentTime = Date.now();
-    matchMeta.matchMakingStartTime=currentTime;
-    matchMeta.matchMakingEndTime=currentTime + matchMeta.matchMakeWaitTime * 1000;
+    matchMeta.matchMakingStartTime = currentTime;
+    matchMeta.matchMakingEndTime = currentTime + matchMeta.matchMakeWaitTime * 1000;
+
+    const playersData: { [userId: string]: PlayerDetailState } = {};
+
     return {
         state: {
-            matchMeta:matchMeta,
-            players:{}
+            matchMeta: matchMeta,
+            players: playersData
         },
         tickRate: 1, // 1 tick per second = 1 MatchLoop func invocations per second
         label: dune_gameName
     };
 }
 
- const Dune_MatchJoinAttempted: nkruntime.MatchJoinAttemptFunction = function (ctx: nkruntime.Context, logger: nkruntime.Logger, nk: nkruntime.Nakama, dispatcher: nkruntime.MatchDispatcher, tick: number, state: nkruntime.MatchState, presence: nkruntime.Presence, metadata: {
+const Dune_MatchJoinAttempted: nkruntime.MatchJoinAttemptFunction = function (ctx: nkruntime.Context, logger: nkruntime.Logger, nk: nkruntime.Nakama, dispatcher: nkruntime.MatchDispatcher, tick: number, state: nkruntime.MatchState, presence: nkruntime.Presence, metadata: {
     [key: string]: any;
-}): { state: nkruntime.MatchState; accept: boolean; rejectMessage?: string | undefined; } | null
-{
+}): { state: nkruntime.MatchState; accept: boolean; rejectMessage?: string | undefined; } | null {
     logger.warn(`TAG:MatchJoinAttempted 1`);
-    let matchMeta:MatchMateState=state.matchMeta;
+    let matchMeta: MatchMateState = state.matchMeta;
     logger.warn(`TAG:MatchJoinAttempted state: ${state.toString()}`);
     logger.warn(`TAG:MatchJoinAttempted metadata:${metadata.toString()}`);
-    if(matchMeta.currentPlayerCount<matchMeta.maxPlayerCount)
-    {
+    if (matchMeta.currentPlayerCount < matchMeta.maxPlayerCount) {
         return {
             state,
             accept: true
         };
     }
-    else
-    {
+    else {
         return {
             state,
             accept: false,
-            rejectMessage:'room is full'
+            rejectMessage: 'room is full'
         };
     }
 }
 
 const Dune_MatchJoin: nkruntime.MatchJoinFunction = function (ctx: nkruntime.Context, logger: nkruntime.Logger, nk: nkruntime.Nakama, dispatcher: nkruntime.MatchDispatcher, tick: number, state: nkruntime.MatchState, presences: nkruntime.Presence[]): {
     state: nkruntime.MatchState
-} | null
-{
-    let matchMeta:MatchMateState=state.matchMeta;
-    matchMeta.currentPlayerCount+=1;
-    if(matchMeta.currentPlayerCount == matchMeta.maxPlayerCount)
-    {
+} | null {
+
+
+    presences.forEach(function (presence) {
+        state.players[presence.userId] = new PlayerDetailState(presence.username, presence.userId, 0, "");
+        logger.debug('%q joined Lobby match', presence.userId);
+    });
+
+
+    let matchMeta: MatchMateState = state.matchMeta;
+    matchMeta.currentPlayerCount += 1;
+    if (matchMeta.currentPlayerCount == matchMeta.maxPlayerCount) {
         // Max player found so starting the match 
-        matchMeta.matchStated==true;
+        matchMeta.matchStated == true;
     }
     logger.warn(`TAG:MatchJoin ${matchMeta}`);
     logger.warn(`TAG:MatchJoin ${state.toString()}`);
@@ -98,65 +97,66 @@ const Dune_MatchJoin: nkruntime.MatchJoinFunction = function (ctx: nkruntime.Con
 
 const Dune_MatchLeave: nkruntime.MatchLeaveFunction = function (ctx: nkruntime.Context, logger: nkruntime.Logger, nk: nkruntime.Nakama, dispatcher: nkruntime.MatchDispatcher, tick: number, state: nkruntime.MatchState, presences: nkruntime.Presence[]): {
     state: nkruntime.MatchState
-} | null
-{
-    let matchMeta:MatchMateState=state.matchMeta;
-    matchMeta.currentPlayerCount-=1;
-    return {state};
+} | null {
+
+    presences.forEach(function (presence) {
+        delete (state.players[presence.userId]);
+        logger.debug('%q left Lobby match', presence.userId);
+    });
+
+    let matchMeta: MatchMateState = state.matchMeta;
+    matchMeta.currentPlayerCount -= 1;
+    return { state };
 }
 
- const Dune_MatchLoop: nkruntime.MatchLoopFunction = function (ctx: nkruntime.Context, logger: nkruntime.Logger, nk: nkruntime.Nakama, dispatcher: nkruntime.MatchDispatcher, tick: number, state: nkruntime.MatchState, messages: nkruntime.MatchMessage[]): {
+const Dune_MatchLoop: nkruntime.MatchLoopFunction = function (ctx: nkruntime.Context, logger: nkruntime.Logger, nk: nkruntime.Nakama, dispatcher: nkruntime.MatchDispatcher, tick: number, state: nkruntime.MatchState, messages: nkruntime.MatchMessage[]): {
     state: nkruntime.MatchState;
-} | null
-{
-    let matchMeta:MatchMateState=state.matchMeta;
-    let currentTime=Date.now();
-    if(matchMeta.matchStated == false)
-    {
-        if(currentTime > matchMeta.matchMakingEndTime)
-        {
+} | null {
+    let matchMeta: MatchMateState = state.matchMeta;
+    let currentTime = Date.now();
+    if (matchMeta.matchStated == false) {
+        if (currentTime > matchMeta.matchMakingEndTime) {
             //Match make waiting time is over
-            if (matchMeta.currentPlayerCount >= matchMeta.minPlayerCount)
-            {
+            if (matchMeta.currentPlayerCount >= matchMeta.minPlayerCount) {
                 // has found minimum required player so starting the match
                 matchMeta.matchStated = true;
                 matchMeta.gamePlayStartTime = currentTime;
-                matchMeta.matchMakingEndTime = currentTime+ matchMeta.gamePlayTime * 1000;
+                matchMeta.matchMakingEndTime = currentTime + matchMeta.gamePlayTime * 1000;
                 logger.warn("TAG::Match Started min player found");
                 return {
                     state
                 }
             }
-            else
-            {
+            else {
                 //TODO write dispatcher message here
                 logger.warn("TAG::Match not found force stop");
                 return null;
             }
         }
-        else
-        {
+        else {
             //Waiting for match making
-            logger.warn(`TAG::Match waiting........${currentTime.toString()}>${matchMeta.matchMakingEndTime.toString()} || ${currentTime>matchMeta.matchMakingEndTime}`);
+            logger.warn(`TAG::Match waiting........${currentTime.toString()}>${matchMeta.matchMakingEndTime.toString()} || ${currentTime > matchMeta.matchMakingEndTime}`);
             return {
                 state
             }
         }
 
     }
-    else
-    {
-        if(currentTime > matchMeta.gamePlayEndTime)
-        {
+    else {
+        if (currentTime > matchMeta.gamePlayEndTime) {
             //Game over due to time out
             logger.warn("TAG::Match over due to time out");
             //TODO write dispatcher message here and conclude game
             return null;
         }
-        else
-        {
+        else {
             //TODO Write Game logic here
             logger.warn("TAG::Match GameLogic");
+            //calculate score and update state
+            messages.forEach(function (message) {
+                logger.info('Received %v from %v', message.data, message.sender.userId);
+                //  dispatcher.broadcastMessage(1, message.data, [message.sender], null);
+            });
             return {
                 state
             }
@@ -167,8 +167,7 @@ const Dune_MatchLeave: nkruntime.MatchLeaveFunction = function (ctx: nkruntime.C
 const Dune_MatchSignal: nkruntime.MatchSignalFunction = function (ctx: nkruntime.Context, logger: nkruntime.Logger, nk: nkruntime.Nakama, dispatcher: nkruntime.MatchDispatcher, tick: number, state: nkruntime.MatchState, data: string): {
     state: nkruntime.MatchState;
     data?: string | undefined;
-} | null
-{
+} | null {
     return {
         state
     }
@@ -176,9 +175,8 @@ const Dune_MatchSignal: nkruntime.MatchSignalFunction = function (ctx: nkruntime
 
 const Dune_MatchTerminate: nkruntime.MatchTerminateFunction = function (ctx: nkruntime.Context, logger: nkruntime.Logger, nk: nkruntime.Nakama, dispatcher: nkruntime.MatchDispatcher, tick: number, state: nkruntime.MatchState, graceSeconds: number): {
     state: nkruntime.MatchState
-} | null
-{
+} | null {
     dune_waitingMap.DeletedByMatchId(ctx.matchId);
     logger.warn(`Tag::serverBase MatchTerminate mmr is ${ctx.matchId}`);
-    return {state};
+    return { state };
 }
--- CommonServer/src/MiscClass.ts
+++ CommonServer/src/MiscClass.ts
@@ -1,180 +1,157 @@
 
-    const emptyResponse = '{responseCode:501,\nmessage:\"emptyResponse or NotImplemented or improper json received \"}';
-    const const_Str_Success='Success';
+const emptyResponse = '{responseCode:501,\nmessage:\"emptyResponse or NotImplemented or improper json received \"}';
+const const_Str_Success = 'Success';
 
-    //<editor-fold desc="gameState Data class">
+//<editor-fold desc="gameState Data class">
 
-    class PlayerDetailState
-    {
-        name: string;
-        avatarId: string;
-        deviceId: string;
-        score: number;
-        constructor(name: string, avatarId: string, score:number, deviceId:string)
-        {
-            this.name = name;
-            this.avatarId = avatarId;
-            this.score=score;
-            this.deviceId=deviceId;
-        }
-        toString(): string
-        {
-            return `{playerName:${this.name},avatarId:${this.avatarId}, score:${this.score}`;
-        }
+class PlayerDetailState {
+    name: string;
+    avatarId: string;
+    deviceId: string;
+    score: number;
+    constructor(name: string, avatarId: string, score: number, deviceId: string) {
+        this.name = name;
+        this.avatarId = avatarId;
+        this.score = score;
+        this.deviceId = deviceId;
+    }
+    toString(): string {
+        return `{playerName:${this.name},avatarId:${this.avatarId}, score:${this.score}`;
+    }
+}
+
+class MatchMateState {
+    roomId: string;
+    minPlayerCount: number;
+    maxPlayerCount: number;
+    currentPlayerCount: number;
+    matchMakeWaitTime: number;
+    matchStated = false;
+    gamePlayTime: number
+    matchMakingStartTime: number;
+    matchMakingEndTime: number;
+    gamePlayStartTime: number;
+    gamePlayEndTime: number;
+
+    constructor(roomId: string, minPlayerCount: number, maxPlayerCount: number, currentPlayerCount: number, matchMakeWaitTime: number, gamePlayTime: number, matchStated: boolean = false) {
+        this.roomId = roomId;
+        this.minPlayerCount = minPlayerCount;
+        this.maxPlayerCount = maxPlayerCount;
+        this.currentPlayerCount = currentPlayerCount;
+        this.matchMakeWaitTime = matchMakeWaitTime;
+        this.matchStated = matchStated;
+        this.gamePlayTime = gamePlayTime;
+        this.matchMakingStartTime = 0;
+        this.gamePlayStartTime = 0;
+        this.matchMakingEndTime = 0;
+        this.gamePlayEndTime = 0;
     }
 
-    class MatchMateState
-    {
-        roomId: string ;
-        minPlayerCount: number;
-        maxPlayerCount: number;
-        currentPlayerCount:number;
-        matchMakeWaitTime: number;
-        matchStated=false;
-        gamePlayTime: number
-        matchMakingStartTime: number;
-        matchMakingEndTime:number;
-        gamePlayStartTime: number;
-        gamePlayEndTime:number;
-
-        constructor(roomId: string, minPlayerCount: number, maxPlayerCount: number, currentPlayerCount: number, matchMakeWaitTime: number, gamePlayTime: number, matchStated: boolean=false)
-        {
-            this.roomId = roomId;
-            this.minPlayerCount = minPlayerCount;
-            this.maxPlayerCount = maxPlayerCount;
-            this.currentPlayerCount = currentPlayerCount;
-            this.matchMakeWaitTime = matchMakeWaitTime;
-            this.matchStated = matchStated;
-            this.gamePlayTime = gamePlayTime;
-            this.matchMakingStartTime=0;
-            this.gamePlayStartTime=0;
-            this.matchMakingEndTime=0;
-            this.gamePlayEndTime=0;
-        }
-
-        toString(): string
-        {
-            return `{roomId:${this.roomId},minPlayerCount:${this.minPlayerCount},
+    toString(): string {
+        return `{roomId:${this.roomId},minPlayerCount:${this.minPlayerCount},
             maxPlayerCount:${this.maxPlayerCount},currentPlayerCount:${this.currentPlayerCount}, 
             matchMakeWaitTime:${this.matchMakeWaitTime},matchStated:${this.matchStated},
             gamePlayTime${this.gamePlayTime}`;
-        }
-
-
     }
-    //</editor-fold>
 
-    //<editor-fold desc="receiving Data class">
 
-    class MatchMakingDetailsReceived
-    {
-        roomId: string ;
-        minPlayerCount: number;
-        maxPlayerCount: number;
-        matchMakeWaitTime: number;
-        gamePlayTime: number
+}
+//</editor-fold>
 
-        constructor(roomId: string, minPlayerCount: number, maxPlayerCount: number, matchMakeWaitTime: number, gamePlayTime: number)
-        {
-            this.roomId = roomId;
-            this.minPlayerCount = minPlayerCount;
-            this.maxPlayerCount = maxPlayerCount;
-            this.matchMakeWaitTime = matchMakeWaitTime;
-            this.gamePlayTime = gamePlayTime;
-        }
+//<editor-fold desc="receiving Data class">
 
-        // Method to display student information
-        toString(): string
-        {
-            return `{roomId: ${this.roomId},minPlayerCount: ${this.minPlayerCount},
+class MatchMakingDetailsReceived {
+    roomId: string;
+    minPlayerCount: number;
+    maxPlayerCount: number;
+    matchMakeWaitTime: number;
+    gamePlayTime: number
+
+    constructor(roomId: string, minPlayerCount: number, maxPlayerCount: number, matchMakeWaitTime: number, gamePlayTime: number) {
+        this.roomId = roomId;
+        this.minPlayerCount = minPlayerCount;
+        this.maxPlayerCount = maxPlayerCount;
+        this.matchMakeWaitTime = matchMakeWaitTime;
+        this.gamePlayTime = gamePlayTime;
+    }
+
+    // Method to display student information
+    toString(): string {
+        return `{roomId: ${this.roomId},minPlayerCount: ${this.minPlayerCount},
             maxPlayerCount: ${this.maxPlayerCount},matchMakeWaitTime: ${this.matchMakeWaitTime}}
             ,gamePlayTime:${this.gamePlayTime}`;
-        }
-
-
-    }
-    //</editor-fold>
-
-    //<editor-fold desc="Response Data class">
-
-    abstract class ApiResponse<T>
-    {
-        responseCode: ResponseCode;
-        message: string;
-        data: T;
-
-        constructor(data: T,responseCode: ResponseCode = ResponseCode.OK, message: string = const_Str_Success)
-        {
-            this.responseCode = responseCode;
-            this.message = message;
-            this.data = data;
-        }
     }
 
-    class MatchMakingResponseData
-    {
-        roomId: string;
-        matchId: string;
-        constructor(roomId: string, matchId: string)
-        {
-            this.roomId = roomId;
-            this.matchId = matchId;
-        }
+
+}
+//</editor-fold>
+
+//<editor-fold desc="Response Data class">
+
+abstract class ApiResponse<T> {
+    responseCode: ResponseCode;
+    message: string;
+    data: T;
+
+    constructor(data: T, responseCode: ResponseCode = ResponseCode.OK, message: string = const_Str_Success) {
+        this.responseCode = responseCode;
+        this.message = message;
+        this.data = data;
+    }
+}
+
+class MatchMakingResponseData {
+    roomId: string;
+    matchId: string;
+    constructor(roomId: string, matchId: string) {
+        this.roomId = roomId;
+        this.matchId = matchId;
+    }
+}
+
+class MatchMakingResponse extends ApiResponse<MatchMakingResponseData> {
+
+}
+//</editor-fold>
+
+//<editor-fold desc="supporting Functional Data class">
+
+class WaitingMatches<T> {
+    public waitingMatchesByRoomId: Map<string, T>;
+    public waitingMatchesByMatchId: Map<string, T>;
+
+    public constructor() {
+        this.waitingMatchesByRoomId = new Map<string, T>();
+        this.waitingMatchesByMatchId = new Map<string, T>();
     }
 
-    class MatchMakingResponse extends ApiResponse<MatchMakingResponseData>
-    {
-
+    public Set(roomId: string, matchId: string, mmr: T): void {
+        this.waitingMatchesByRoomId.set(roomId, mmr);
+        this.waitingMatchesByMatchId.set(matchId, mmr);
     }
-    //</editor-fold>
 
-    //<editor-fold desc="supporting Functional Data class">
-
-    class WaitingMatches<T>
-    {
-        public waitingMatchesByRoomId: Map<string, T>;
-        public waitingMatchesByMatchId: Map<string, T>;
-
-        public constructor()
-        {
-            this.waitingMatchesByRoomId = new Map<string, T>();
-            this.waitingMatchesByMatchId = new Map<string, T>();
+    public GetMMRByRoomId(roomId: string): T | undefined {
+        if (this.waitingMatchesByRoomId.has(roomId)) {
+            return this.waitingMatchesByRoomId.get(roomId);
         }
-
-        public Set(roomId: string, matchId: string, mmr: T): void
-        {
-            this.waitingMatchesByRoomId.set(roomId, mmr);
-            this.waitingMatchesByMatchId.set(matchId, mmr);
-        }
-
-        public GetMMRByRoomId(roomId: string): T | undefined
-        {
-            if (this.waitingMatchesByRoomId.has(roomId))
-            {
-                return this.waitingMatchesByRoomId.get(roomId);
-            }
-            return undefined;
-        }
-
-        public GetMMRByMatchId(matchId: string): T | undefined
-        {
-            if (this.waitingMatchesByMatchId.has(matchId))
-            {
-                return this.waitingMatchesByMatchId.get(matchId);
-            }
-            return undefined;
-        }
-
-        public DeletedByRoomId(matchId: string): boolean
-        {
-            this.waitingMatchesByMatchId.delete(matchId)
-            return this.waitingMatchesByRoomId.delete(matchId);
-        }
-
-        public DeletedByMatchId(matchId: string): boolean
-        {
-            this.waitingMatchesByRoomId.delete(matchId)
-            return this.waitingMatchesByMatchId.delete(matchId);
-        }
+        return undefined;
     }
-    //</editor-fold>
+
+    public GetMMRByMatchId(matchId: string): T | undefined {
+        if (this.waitingMatchesByMatchId.has(matchId)) {
+            return this.waitingMatchesByMatchId.get(matchId);
+        }
+        return undefined;
+    }
+
+    public DeletedByRoomId(matchId: string): boolean {
+        this.waitingMatchesByMatchId.delete(matchId)
+        return this.waitingMatchesByRoomId.delete(matchId);
+    }
+
+    public DeletedByMatchId(matchId: string): boolean {
+        this.waitingMatchesByRoomId.delete(matchId)
+        return this.waitingMatchesByMatchId.delete(matchId);
+    }
+}
+//</editor-fold>
